e3cddc71af466601b41b57525562cb9dbcdfc19c
Andru Mocanu <andru.mocanu@emag.ro>
2017-09-09 20:52:28 +0300

    listener done

--- app/src/main/java/vcrms/marcel/assistant.java
+++ app/src/main/java/vcrms/marcel/assistant.java
@@ -1,13 +1,64 @@
 package vcrms.marcel;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class assistant extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class Assistant extends AppCompatActivity {
+
+    private Button speakBtn;
+    private TextView recText;
+    private final int REQ_CODE_SPEECH_OUTPUT = 143;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assistant);
+
+        speakBtn = (Button) findViewById(R.id.speakBtn);
+        recText = (TextView) findViewById(R.id.recText);
+
+        speakBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnToOpenMic();
+            }
+        });
+    }
+
+    private void btnToOpenMic() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hiii speak Now....");
+
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_OUTPUT);
+        } catch (ActivityNotFoundException tim) {
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_OUTPUT:
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> voiceInText = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    recText.setText(voiceInText.get(0));
+                }
+                break;
+        }
     }
 }
