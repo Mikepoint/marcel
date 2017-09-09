a1be36935ac471498387bccf4d977ddee4f54216
Mihai Puiu | eMAG <mihai.puiu@emag.ro>
2017-09-09 20:38:46 +0300

    initial comm

---
+++ app/src/main/java/vcrms/marcel/assistant.java
@@ -0,0 +1,13 @@
+package vcrms.marcel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class assistant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assistant);
+    }
+}
